ffe7eef60a38dff81c15f575795f2dbb1ad89ea5
Tailor17 <tailorzitzke.pl013@academico.ifsul.edu.br>
2026-09-17 22:14:19 -0300

    fix: Criação de rotas manuais, substiuindo o resource.

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,38 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/pedidos', [PedidoController::class, 'index'])->name('pedidos.index');
+Route::get('/pedidos/create', [PedidoController::class, 'create'])->name('pedidos.create');
+Route::post('/pedidos', [PedidoController::class, 'store'])->name('pedidos.store');
+Route::get('/pedidos/{pedido}', [PedidoController::class, 'show'])->name('pedidos.show');
+Route::get('/pedidos/{pedido}/edit', [PedidoController::class, 'edit'])->name('pedidos.edit');
+Route::put('/pedidos/{pedido}', [PedidoController::class, 'update'])->name('pedidos.update');
+Route::delete('/pedidos/{pedido}', [PedidoController::class, 'destroy'])->name('pedidos.destroy');
+
+Route::get('/produtos', [ProdutoController::class, 'index'])->name('produtos.index');
+Route::get('/produtos/create', [ProdutoController::class, 'create'])->name('produtos.create');
+Route::post('/produtos', [ProdutoController::class, 'store'])->name('produtos.store');
+Route::get('/produtos/{produto}', [ProdutoController::class, 'show'])->name('produtos.show');
+Route::get('/produtos/{produto}/edit', [ProdutoController::class, 'edit'])->name('produtos.edit');
+Route::put('/produtos/{produto}', [ProdutoController::class, 'update'])->name('produtos.update');
+Route::delete('/produtos/{produto}', [ProdutoController::class, 'destroy'])->name('produtos.destroy');
+
+Route::get('/clientes', [ClienteController::class, 'index'])->name('clientes.index');
+Route::get('/clientes/create', [ClienteController::class, 'create'])->name('clientes.create');
+Route::post('/clientes', [ClienteController::class, 'store'])->name('clientes.store');
+Route::get('/clientes/{cliente}', [ClienteController::class, 'show'])->name('clientes.show');
+Route::get('/clientes/{cliente}/edit', [ClienteController::class, 'edit'])->name('clientes.edit');
+Route::put('/clientes/{cliente}', [ClienteController::class, 'update'])->name('clientes.update');
+Route::delete('/clientes/{cliente}', [ClienteController::class, 'destroy'])->name('clientes.destroy');
+Route::delete('/clientes/{cliente}', [ClienteController::class, 'destroy'])->name('clientes.destroy');
+
+
+
+
+
+
+/*
 Route::resource('produtos', ProdutoController::class);
 Route::resource('clientes', ClienteController::class);
 Route::resource('pedidos', PedidoController::class);
+*/
